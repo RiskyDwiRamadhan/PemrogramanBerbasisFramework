7f0c6562225c37135db0ca2ef1fbe262351e58ba
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2022-04-14 10:12:41 +0700

    Pertemuan 8

--- Pertemuan8/uts/src/component/ShopPost.jsx
+++ Pertemuan8/uts/src/component/ShopPost.jsx
@@ -20,27 +20,6 @@ class ShopPost extends Component{
 		this.ambilDataDariServerAPI()
 	}
 
-	handleHapusProduct = (data) =>{
-		fetch(`http://localhost:3001/product/${data}`, {method:'DELETE'})
-			.then(res =>{
-				this.ambilDataDariServerAPI()
-			})
-	}
-
-	handleTombolSimpan = () => {
-		fetch('http://localhost:3002/keranjang', {
-			method: 'post',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(this.state.insertKranjang)
-		})
-			.then((Response) => {
-				this.ambilDataDariServerAPI();
-			});
-	}
-
 	render(){
 		return(
 			<div className="post-Product">
@@ -48,7 +27,7 @@ class ShopPost extends Component{
 			 	{
 					this.state.listProduct.map(Product => {
 						return <Shop key={Product.id} pid={Product.id} Pname={Product.nama} Pharga={Product.harga} 
-									 Pgambar={Product.gambar} Pstok={Product.stok} beliProduct={this.handleTombolSimpan}/>
+									 Pgambar={Product.gambar} Pstok={Product.stok}/>
 			 		})
 			 	}
 			</div>
