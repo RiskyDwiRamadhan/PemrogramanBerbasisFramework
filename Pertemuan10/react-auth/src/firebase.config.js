3ae83ac27a32a2186ed74e591c93ebd2b047b469
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2022-05-19 20:07:21 +0700

    Pertemuan 10

---
+++ Pertemuan10/react-auth/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAevKfXcsrZDTjoaIH_yQccaRCCo_paoK4",
+    authDomain: "praktikum-b7222.firebaseapp.com",
+    projectId: "praktikum-b7222",
+    storageBucket: "praktikum-b7222.appspot.com",
+    messagingSenderId: "150789753257",
+    appId: "1:150789753257:web:f12da60b46973500f49cb8",
+    measurementId: "G-XK6SJBD2ZL"
+}; 
